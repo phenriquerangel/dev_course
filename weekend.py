5736c41aca9e85d3d41444b927b75da918cafdf1
phenriquerangel <phenriquerangel@gmail.com>
2019-04-25 00:58:23 +0000

    Finish ex 12

---
+++ weekend.py
@@ -0,0 +1,32 @@
+days=int(input("Day of week: "))
+
+days = {
+    1:"Sunday",
+    2:"Monday",
+    3:"Tuesday",
+    4:"Wednesday",
+    5:"Thursday",
+    6:"Friday",
+    7:"Satuday"    
+    }
+
+for x in days(key,value):
+    print(value)
+
+
+'''
+if day == 1:
+    print("Sunday")
+elif day == 2:
+    print("Monday")
+elif day == 3:
+    print("Tuesday")
+elif day == 4:
+    print("Wednesday")
+elif day == 5:
+    print("Thursday")
+elif day == 6:
+    print("Friday")
+elif day == 7:
+    print("Saturday")
+'''
